ab74ed9bab056c49c99134e68af2a8f038d53a5e
Karan <>
2024-06-13 12:18:20 +0530

    fake db

--- main.py
+++ main.py
@@ -3,11 +3,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+books = [
+    {"book_title": "book 1", "author": "author 1"},
+    {"book_title": "book 2", "author": "author 1"},
+    {"book_title": "book 3", "author": "author 2"},
+]
+
 
 # http://localhost:8000/
 @app.route("/")
 def hello():
     return render_template("index.html")
+
+
+@app.route("/books")
+def book_list():
+    book = {"book_title": "book 1", "author": "author 1"}
+    return render_template("book_list.html", instance=book)
 
 
 if __name__ == "__main__":
