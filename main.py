0ce812169452a6eca5d703387c4282fbbd72f52f
Karan <>
2024-06-16 11:32:46 +0530

    Create and List View for User entity is done; Added Form to create new user.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from db import db
 from models import User
 
@@ -25,10 +25,51 @@ def book_list():
     return render_template("book_list.html", books=books)
 
 
-@app.route("/users")
+# List - (We are getting an HTTP GET request to this endpoint)
+@app.route("/users", methods=["GET", "POST"])
 def list_users():
-    users = User.query.all()
-    return render_template("user_list.html", users=users)
+    if request.method == "GET":
+        users = User.query.all()
+        return render_template("user_list.html", users=users)
+    elif request.method == "POST":
+        # 1. Validate and get request data
+        username = request.form.get("username")  # None if no key named 'username'
+        email = request.form.get("email")
+        password = request.form.get("password")
+        print(f"username: {username}, password: {password}")
+        role = "student"
+        # validate the request data for our usage
+
+        # 2. Create an entry to the users table
+        new_user = User(username=username, email=email, password=password, role=role)
+        db.session.add(new_user)
+        db.session.commit()
+        # 3. Send appropriate response
+        return redirect(url_for("list_users"))
+
+
+# Create - We will send HTTP POST request with the form data to create a new entity
+# @app.route("/users")
+# def create_user(request):
+#     pass
+
+
+# Retrieve
+@app.route("/users/<user_id>")
+def get_user(user_id):
+    pass
+
+
+# Update
+@app.route("/users/<user_id>")
+def update_user(user_id):
+    pass
+
+
+# Delete
+@app.route("/users/<user_id>")
+def delete_user(user_id):
+    pass
 
 
 if __name__ == "__main__":
