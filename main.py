11d7a2519e5c25096611dc2ec4cd27d97fbde3a3
Karan <>
2024-06-17 22:45:43 +0530

    worked on styling login/register forms, added section model

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
+from sqlalchemy.exc import NoResultFound
 from db import db
-from models import User
+from models import User, Book
 
 app = Flask(__name__)
 
-books = [
-    {"book_title": "book 1", "author": "author 1"},
-    {"book_title": "book 2", "author": "author 1"},
-    {"book_title": "book 3", "author": "author 2"},
-]
 
+app.config["SECRET_KEY"] = "some_secret"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 db.init_app(app)
 
@@ -17,12 +14,59 @@ db.init_app(app)
 # http://localhost:8000/
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    books = Book.query.all()
+    return render_template("homepage.html", books=books)
 
 
-@app.route("/books")
-def book_list():
-    return render_template("book_list.html", books=books)
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("users/register.html")
+    elif request.method == "POST":
+        # 1. Validate and get request data
+        username = request.form.get("username")  # None if no key named 'username'
+
+        email = request.form.get("email")
+        password = request.form.get("password")
+        print(f"username: {username}, password: {password}")
+        role = "student"
+        # validate the request data for our usage
+
+        # 2. Create an entry to the users table
+        new_user = User(username=username, email=email, password=password, role=role)
+        db.session.add(new_user)
+        db.session.commit()
+        # 3. Send appropriate response
+        return redirect(url_for("login"))
+
+
+def check_password(user_password, password):
+    return user_password == password
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("users/login.html")
+    elif request.method == "POST":
+        # 1. Validate and get request data
+        username = request.form.get("username")  # None if no key named 'username'
+        password = request.form.get("password")
+        print(f"username: {username}, password: {password}")
+        # validate the request data for our usage
+
+        # 2. fetch details for the username
+        try:
+            user = User.query.filter_by(username=username).one()
+        except NoResultFound as e:
+            flash("User with this username not found.", "warning")
+            return redirect(url_for("login"))
+        authenticated = check_password(user.password, password)
+        if authenticated:
+            return redirect(url_for("hello"))
+        else:
+            flash("Password is wrong.", "warning")
+            return redirect(url_for("login"))
 
 
 # List - (We are getting an HTTP GET request to this endpoint)
@@ -73,6 +117,6 @@ def delete_user(user_id):
 
 
 if __name__ == "__main__":
-    # with app.app_context():
-    #     db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(host="localhost", port=8000, debug=True)
--- models.py
+++ models.py
@@ -8,3 +8,15 @@ class User(db.Model):
     password = db.Column(db.String, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
     role = db.Column(db.String)
+
+
+class Book(db.Model):
+    __tablename__ = "books"
+    book_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    section = db.Column(db.String, nullable=True)
+    # pages
+    # thumbnail
+    #
